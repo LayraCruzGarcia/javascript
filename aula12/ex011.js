6b96e5bab310c4fb5e38ff2ae5d1f0aecdd15a91
LayraCruzGarcia <layra.garcia18@gmail.com>
2023-11-28 11:10:12 -0300

    Aula 12

---
+++ aula12/ex011.js
@@ -0,0 +1,17 @@
+/*Teste no Nodejs*/
+var idade = 22
+if (idade < 18){
+    console.log('Menor de idade')
+} else {
+    console.log('Maior de idade')
+}
+
+
+var idade = 22
+if (idade < 18) {
+    console.log('Adolescente')
+} else if (idade < 13) {
+    console.log('Criança')
+} else if (idade > 18) {
+    console.log('Adulto')
+}
